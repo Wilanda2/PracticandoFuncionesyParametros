a729292c539dc2c652dfc9359112e445e20d5e27
Wilanda2 <yolanda.lopezdelarica@gmail.com>
2024-01-11 18:55:58 +0100

    Function ten created

---
+++ main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+//Fase 1
+//Vamos a hacer una función que escriba patata 10 veces en la consola la función se llamará ten()
+
+
+
+//Primero se declara la función, el nombre que tiene y lo que hace.
+function ten() {
+    //con un bucle for (inicialización, condición y actualización)
+    for (let i = 0; i < 10; i++) {
+    console.log('patata');
+  }
+}
+//Después se ejecuta, hay que guardarla en una constante nueva que recogerá el valor que se genera.
+
+const tenPotatoes = ten();
+
